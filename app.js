e8a1ec536511c85b9dc015ddfcfd9b49387f3b63
Harshit <cr273916@gmail.com>
2025-09-24 19:55:51 +0530

    app

--- app.js
+++ app.js
@@ -1,26 +1,12 @@
-// const express = require('express');
-// const app = express();
-// const authRoutes = require('./Routes/authRoutes');
-
-// app.use(express.json()); // Parse JSON bodies
-
-// // Routes
-// app.use('/api', authRoutes);
-
-// // Start server
-// const PORT = 3000;
-// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
-
-
 const express = require('express');
-const cors = require('cors');
-const authRoutes = require('./Routes/authRoutes');
 const app = express();
+const authRoutes = require('./Routes/authRoutes');
 
-app.use(cors());
-app.use(express.json());
+app.use(express.json()); // Parse JSON bodies
+
+// Routes
 app.use('/api', authRoutes);
 
-const PORT = process.env.PORT || 3000;
+// Start server
+const PORT = 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
