bb64d94e9eb2714702249c7a48f5cb63ffbf7688
Harshit <cr273916@gmail.com>
2025-09-24 18:06:15 +0530

    utlis

--- Utils/fileUtils.js
+++ Utils/fileUtils.js
@@ -4,8 +4,13 @@ const path = require('path');
 const filePath = path.join(__dirname, '../Data/users.json');
 
 function readUsers() {
-  const data = fs.readFileSync(filePath, 'utf-8');
-  return JSON.parse(data);
+  // create file if it doesn't exist
+  if (!fs.existsSync(filePath)) {
+    fs.writeFileSync(filePath, "[]");
+  }
+
+  const data = fs.readFileSync(filePath, "utf-8");
+  return JSON.parse(data); // return the users array
 }
 
 function writeUsers(users) {
